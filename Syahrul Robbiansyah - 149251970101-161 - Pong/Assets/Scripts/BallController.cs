4915e014206025a35e6df0189f54c3ed72020502
Syahrul Robbiansyah <61303093+robbysrr@users.noreply.github.com>
2022-06-15 23:37:06 +0700

    Base gameplay
    Base gameplay

---
+++ Syahrul Robbiansyah - 149251970101-161 - Pong/Assets/Scripts/BallController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public Vector2 speed;
+
+    private Rigidbody2D rig;
+
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        rig.velocity = speed;
+    }
+
+}
---
+++ Syahrul Robbiansyah - 149251970101-161 - Pong/Assets/Scripts/PaddleController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour
+{
+     public int speed;
+     public KeyCode upkey;
+     public KeyCode downkey;
+     private Rigidbody2D rig;
+
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        MoveObject(GetInput());
+    }
+
+    // input
+    private Vector2 GetInput()
+    {
+        
+
+        if (Input.GetKey(upkey))
+        {
+            return Vector2.up * speed; // up movement
+        }
+        else if (Input.GetKey(downkey))
+        {
+            return Vector2.down * speed; // down movement
+        }
+
+        return Vector2.zero;
+
+    }
+
+    // object movement using input
+    private void MoveObject(Vector2 movement)
+    {
+        rig.velocity = movement;
+    }
+}
