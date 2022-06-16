1c27ce388919ef7e3c0ed7f174929f885ff54982
Syahrul Robbiansyah <61303093+robbysrr@users.noreply.github.com>
2022-06-16 18:02:33 +0700

    Reupload Main Menu Scene
    Main Menu scene with title, play button, and author button debug

---
+++ Syahrul Robbiansyah - 149251970101-161 - Pong/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    public void OpenAuthor()
+    {
+        Debug.Log("Created by robby_srr");
+    }
+}
